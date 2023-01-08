aa7265147ac5f2b30ba71bc8cd11b95cf7cf2c3a
Roberto Bianchi <roberto.bianki@gmail.com>
2023-01-08 12:00:28 +0100

    Add test case for special names of cols
    Signed-off-by: Roberto Bianchi <roberto.bianki@gmail.com>

--- migrations/001.do.sql
+++ migrations/001.do.sql
@@ -1,13 +1,16 @@
-CREATE SCHEMA IF NOT EXISTS myschema;
+CREATE TABLE IF NOT EXISTS abc (
+  id INTEGER NOT NULL,
+  __special_id__ INTEGER,
+  PRIMARY KEY (id)
+);
 
-CREATE TABLE IF NOT EXISTS myschema.table1 (
+CREATE TABLE IF NOT EXISTS __def__ (
   id INTEGER NOT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS myschema.table2 (
+CREATE TABLE IF NOT EXISTS ghi (
   id INTEGER NOT NULL,
-  fk_id INTEGER NOT NULL,
-  CONSTRAINT table1_fk_id FOREIGN KEY (fk_id) REFERENCES myschema.table1 (id),
+  __a0__ INTEGER,
   PRIMARY KEY (id)
 );
