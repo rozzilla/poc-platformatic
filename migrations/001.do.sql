9b537c67a2b9a4af15ca6dee85044f11edd31169
Roberto Bianchi <roberto.bianchi@spendesk.com>
2023-01-13 16:33:44 +0100

    Recreate failure
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>

--- migrations/001.do.sql
+++ migrations/001.do.sql
@@ -1,21 +1,32 @@
-CREATE TABLE IF NOT EXISTS abc (
-  id INTEGER NOT NULL,
-  __special_id__ INTEGER,
-  PRIMARY KEY (id)
+CREATE SCHEMA test1;
+CREATE SCHEMA test2;
+CREATE SCHEMA test3;
+CREATE SCHEMA test4;
+
+CREATE TABLE test1.authors (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(42)
 );
 
-CREATE TABLE IF NOT EXISTS __def__ (
-  id INTEGER NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE test2.books (
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(42),
+  author_id INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS ghi (
-  id INTEGER NOT NULL,
-  __a0__ INTEGER,
-  PRIMARY KEY (id)
+CREATE TABLE test3.authors (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(42)
 );
 
-CREATE TABLE IF NOT EXISTS __lmn__ (
-  __id__ INTEGER NOT NULL,
-  PRIMARY KEY (__id__)
+CREATE TABLE test4.books (
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(42),
+  author_id INTEGER
 );
+
+ALTER TABLE ONLY test2.books
+  ADD CONSTRAINT authors_fkey FOREIGN KEY (author_id) REFERENCES test1.authors(id);
+
+ALTER TABLE ONLY test4.books
+  ADD CONSTRAINT authors_fkey FOREIGN KEY (author_id) REFERENCES test3.authors(id);
