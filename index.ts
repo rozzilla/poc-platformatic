fcf3f0b7fff21614bee0c3a6dbd4cda24ebe5aa5
Roberto Bianchi <roberto.bianchi@spendesk.com>
2023-02-23 11:23:19 +0100

    Update log

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import { loggerPlugin } from "./src/plugins/logger";
 import { random } from "./src/hooks/random";
 
 export default async (app: FastifyInstance) => {
+  app.log.info("here we go!");
   app.addHook("preParsing", random);
 
   app.register(loggerPlugin, { customValue: 42 });
